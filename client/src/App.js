53a7c91f04e2c613e0167bd4799a010fec4090c0
JoesGitCode <jsyoung0891@gmail.com>
2021-03-21 16:38:53 +0000

    Add Bootstrap. Basic navbar, footbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React, { Fragment } from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <Fragment>
-      <h1>Yoooo</h1>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <h1>PokeCard</h1>
+        </Container>
+      </main>
+      <Footer/>
     </Fragment>
   );
 }
-
 export default App;
